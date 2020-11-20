4fc97e065f4b5966e287f7e32f68d84038529a0e
iN4sser <iN4sser@users.noreply.github.com>
2020-11-20 11:36:38 +0300

    Update WhatsappWebAutoSender.user.js

--- WhatsappWebAutoSender.user.js
+++ WhatsappWebAutoSender.user.js
@@ -14,6 +14,6 @@ window.onload=function(){
 
 }
 function autoClick(){   
-  document.getElementsByClassName('_1U1xa')[0].click();
+  document.getElementsByClassName('_2Ujuu')[0].click();
   setTimeout(window.close,5000);
 }
