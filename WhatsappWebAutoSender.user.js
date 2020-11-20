550c0182966f7742fe290361c7c440c792e6efad
iN4sser <iN4sser@users.noreply.github.com>
2020-11-20 11:51:19 +0300

    Update WhatsappWebAutoSender.user.js

--- WhatsappWebAutoSender.user.js
+++ WhatsappWebAutoSender.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name        Whatsapp Web Auto Sender
 // @namespace   iN4sser
-// @match       http*://web.whatsapp.com/*
+// @match       *://web.whatsapp.com/*
 // @grant       window.close
-// @version     1.2
+// @version     1.3
 // @author      iN4sser
 // @updateURL   https://raw.githubusercontent.com/iN4sser/Whatsapp-Web-Auto-Sender/master/WhatsappWebAutoSender.user.js
+// @downloadURL https://raw.githubusercontent.com/iN4sser/Whatsapp-Web-Auto-Sender/master/WhatsappWebAutoSender.user.js
 // @description 3/12/2020, 12:29:32 PM
 // ==/UserScript==
 window.onload=function(){
