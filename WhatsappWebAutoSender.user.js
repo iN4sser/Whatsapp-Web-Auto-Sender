d4079955c2dc6b57e2ebc478dd70df072825c8d2
iN4sser <iN4sser@users.noreply.github.com>
2022-05-11 15:02:29 +0300

    Update WhatsappWebAutoSender.user.js

--- WhatsappWebAutoSender.user.js
+++ WhatsappWebAutoSender.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://www.in4sser.com
 // @match       *://web.whatsapp.com/*
 // @grant       window.close
-// @version     1.5
+// @version     1.6
 // @author      iN4sser
 // @updateURL   https://github.com/iN4sser/Whatsapp-Web-Auto-Sender/raw/master/WhatsappWebAutoSender.user.js
 // @downloadURL https://github.com/iN4sser/Whatsapp-Web-Auto-Sender/raw/master/WhatsappWebAutoSender.user.js
@@ -15,6 +15,6 @@ window.onload=function(){
 
 }
 function autoClick(){   
-  document.getElementsByClassName('_4sWnG')[0].click();
+  document.querySelector("#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._2lMWa > div._3HQNh._1Ae7k > button").click();
   setTimeout(window.close,5000);
 }
